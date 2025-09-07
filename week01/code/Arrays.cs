b9dfe230f9f3e25a675839259b060f71516161bb
princegodwyll15 <godwyllprince@gmail.com>
2025-09-07 15:28:09 +0000

    rotation and multiple algorithm solved

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -12,8 +12,16 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+        //initialize a new array to contain the multiples
+        double[] multiplesArray = new double[length];
+        //create a for loop so that we run the multiples for the exact length specified
+        for (int i = 0; i <= length; i++)
+        {
+            //insert the first multiple value to the first index of our collecting array
+            multiplesArray[i] = number * (i + 1);
+        }
 
-        return []; // replace this return statement with your own
+        return multiplesArray; // replace this return statement with your own
     }
 
     /// <summary>
@@ -25,9 +33,28 @@ public static class Arrays
     /// </summary>
     public static void RotateListRight(List<int> data, int amount)
     {
+        int numberOfData = data.Count;
+        if (amount == 0) return;
+
+        List<int> rotatedArray = new List<int>();
+
+        //take the last amount of items specified and populate it to the rotated array
+        for (int i = numberOfData - amount; i < numberOfData; i++)
+        {
+            rotatedArray.Add(data[i]);
+        }
+
+        //insert remaining part of data into the rotated array
+        for (int i = 0; i < numberOfData - amount; i++)
+        {
+            rotatedArray.Add(data[i]);
+        }
+        data.Clear();
+        data.AddRange(rotatedArray);
         // TODO Problem 2 Start
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
     }
 }
--- week01/teach/ArraySelector.cs
+++ week01/teach/ArraySelector.cs
@@ -3,14 +3,35 @@ public static class ArraySelector
     public static void Run()
     {
         var l1 = new[] { 1, 2, 3, 4, 5 };
-        var l2 = new[] { 2, 4, 6, 8, 10};
-        var select = new[] { 1, 1, 1, 2, 2, 1, 2, 2, 2, 1};
+        var l2 = new[] { 2, 4, 6, 8, 10 };
+        var select = new[] { 1, 1, 1, 2, 2, 1, 2, 2, 2, 1 };
         var intResult = ListSelector(l1, l2, select);
         Console.WriteLine("<int[]>{" + string.Join(", ", intResult) + "}"); // <int[]>{1, 2, 3, 2, 4, 4, 6, 8, 10, 5}
     }
 
     private static int[] ListSelector(int[] list1, int[] list2, int[] select)
     {
-        return [];
+        int array1Index = 0;
+        int array2Index = 0;
+        int[] newArray = new int[select.Length];
+
+        for (int i = 0; i < select.Length; i++)
+        {
+            int selectIndex = select[i];
+            if (selectIndex == 1)
+            {
+                newArray[i] = list1[array1Index];
+                array1Index++;
+            }
+            else
+            {
+                newArray[i] = list2[array2Index];
+                array2Index++;
+            }
+        }
+        {
+
+        }
+        return newArray;
     }
 }
